34c9230042a393198d95aea8fe85933833f33372
Edward Weymouth <14878694+ed42311@users.noreply.github.com>
2021-09-16 17:43:08 -0600

    build: this

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID">
+      <img :src="home.images[0]" style="width: 200px" />
+      <br />
+      {{ home.title }}
+      <br />
+      {{ home.location.address }} {{ home.location.city }}
+      {{ home.location.state }}
+      <br />
+      {{ home.title }}
+      <br />
+    </div>
+  </div>
+</template>
+<script>
+import homes from "~/data/homes";
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
