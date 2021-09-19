fcc4173ad8e00cd313e17ce9bd1df8b7cb19ff33
Edward Weymouth <14878694+ed42311@users.noreply.github.com>
2021-09-19 15:27:11 -0600

    build: setup price per

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <div v-for="home in homes" :key="home.objectID">
-      <img :src="home.images[0]" style="width: 200px" />
+  <div class="home-container">
+    <div v-for="home in homes" :key="home.objectID" class="home">
+      <img class="home-image" :src="home.images[0]" style="width: 200px" />
       <br />
       {{ home.title }}
       <br />
@@ -10,17 +10,32 @@
       <br />
       {{ home.title }}
       <br />
+      {{ home.guests }} Guests
+      <br />
+      {{ home.pricePerNight }} / night
     </div>
   </div>
 </template>
 <script>
-import homes from "~/data/homes";
+import homes from '~/data/homes'
 
 export default {
   data() {
     return {
       homes: homes.slice(0, 3),
-    };
+    }
   },
-};
+}
 </script>
+
+<style scoped>
+.home-container {
+  display: flex;
+}
+.home {
+  padding: 10px;
+}
+.home-image {
+  padding: 10px;
+}
+</style>
